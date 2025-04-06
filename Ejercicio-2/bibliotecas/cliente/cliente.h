1bfdad9ecb542d2bafe8d9930f72972d9995c49a
agusbrocani <aguss.brocani@gmail.com>
2025-04-06 17:28:14 -0300

    Agrego proyecto base de Ejercicio-2.

---
+++ Ejercicio-2/bibliotecas/cliente/cliente.h
@@ -0,0 +1,13 @@
+#ifndef CLIENTE_H_INCLUDED
+#define CLIENTE_H_INCLUDED
+#include <iostream>
+using namespace std;
+
+/**
+    cd /mnt/c/Users/PC/Desktop/Sistemas-Operativos/Ejercicio-2
+    cambiar directorio a una carpeta específica en disco C: de Windows, pero vista desde Ubuntu (WSL).
+    /mnt/c/: En WSL, así se accede al disco C: de Windows.
+*/
+
+
+#endif // CLIENTE_H_INCLUDED
---
+++ Ejercicio-2/servidor.cpp
@@ -0,0 +1,7 @@
+#include "./bibliotecas/servidor/servidor.h"
+
+int main()
+{
+    cout << "Hello, soy el servidor" << endl;
+    return 0;
+}
